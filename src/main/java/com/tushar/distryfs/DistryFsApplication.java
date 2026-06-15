2240237e6ebf049bf717edeea6afbee46dc01e14
tkciitj <b23bb1042@iitj.ac.in>
2026-06-15 22:56:57 +0530

    IntelliJ Idea project push

---
+++ src/main/java/com/tushar/distryfs/DistryFsApplication.java
@@ -0,0 +1,13 @@
+package com.tushar.distryfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistryFsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistryFsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tushar/distryfs/DistryFsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tushar.distryfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistryFsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
